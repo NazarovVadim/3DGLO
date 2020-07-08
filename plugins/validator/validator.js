8bb8781a54fb9465670d590308b14f4c5fc6b95d
Vadim Nazarov <vadim-nazarov-web@yandex.ru>
2020-07-08 13:50:14 +0300

    25

--- plugins/validator/validator.js
+++ plugins/validator/validator.js
@@ -1,103 +1,115 @@
-class Validator{
-    constructor({selector, pattern = {}, method}){
+'use strict';
+
+class Validator {
+    constructor({ selector, pattern = {}, method }) {
         this.form = document.querySelector(selector);
         this.pattern = pattern;
         this.method = method;
-        this.elementsForm = [...this.form.elements].filter(item => item.tagName.toLowerCase() !== 'button'
-                                                                                && item.type !== 'button');
+        this.elementsForm = [...this.form.elements].filter(item => {
+            return item.tagName.toLowerCase() !== 'button' && item.type !== 'button';
+        });
         this.error = new Set();
     }
 
-    init(){
+    init() {
         this.applyStyle();
         this.setPattern();
         this.elementsForm.forEach(item => item.addEventListener('change', this.checkIt.bind(this)));
         this.form.addEventListener('submit', e => {
-            this.elementsForm.forEach(item => this.checkIt({target: item}));
-            if(this.error.size)
+            this.elementsForm.forEach(item => this.checkIt({ target: item }));
+            if (this.error.size)
                 e.preventDefault();
         })
     }
 
-    isValid(elem){
+    isValid(elem) {
         const validatorMathod = {
-            notEmpty(elem){
-                if(elem.value.trim() = ''){
+            notEmpty(elem) {
+                if (elem.value.trim() === '') {
                     return false;
                 }
                 return true;
             },
-            pattern(elem, pattern){
+            pattern(elem, pattern) {
                 return pattern.test(elem.value);
             }
         };
 
-        if(this.method){
+        if (this.method) {
             const method = this.method[elem.id];
 
-            if(method){
+            if (method) {
                 return method.every(item => validatorMathod[item[0]](elem, this.pattern[item[1]]));
             }
         }
         return true;
     }
 
-    checkIt(event){
+    checkIt(event) {
         const target = event.target;
 
-        if(this.isValid(target)){
+        if (this.isValid(target)) {
             this.showSuccess(target);
             this.error.delete(target);
-        } else{
+        } else {
             this.showError(target);
             this.error.add(target);
         }
     }
 
-    showError(elem){
+    showError(elem) {
         elem.classList.remove('success');
         elem.classList.add('error');
-        if(elem.nextElementSibling && elem.nextElementSibling.classList.contains('validator-error')){
+        if (elem.nextElementSibling && elem.nextElementSibling.classList.contains('validator-error')) {
             return;
         }
         const errorDiv = document.createElement('div');
         errorDiv.textContent = 'Ошибка в этом поле!';
         errorDiv.classList.add('validator-error');
-        elem.iinsertAdjacentElement('afterend', errorDiv);
+        elem.insertAdjacentElement('afterend', errorDiv);
     }
 
-    showSuccess(elem){
+    showSuccess(elem) {
         elem.classList.remove('error');
         elem.classList.add('success');
-        if(elem.nextElementSibling && elem.nextElementSibling.classList.contains('validator-error')){
+        if (elem.nextElementSibling && elem.nextElementSibling.classList.contains('validator-error')) {
             elem.nextElementSibling.remove();
         }
     }
 
-    applyStyle(){
+    applyStyle() {
         const style = document.createElement('style');
 
         style.textContent = `
-            input.success{
-                border: solid green;
+            body form input.success {
+                border: 2px solid green !important;
             }
-            input.error{
-                border: solid red;
+            body form input.error {
+                border: 2px solid red !important;
             }
-            .validator-error{
-                font-size: 12px;
-                font-family: sans-serif;
-                color: red;
+            .validator-error {
+                font-size: 14px;
+                color: red;    
+                z-index: 1;
+                position: relative;
             }
         `;
         document.head.appendChild(style);
     }
 
-    setPattern(){
-        this.pattern.phone = this.pattern.phone ? this.pattern.phone : /^\+?[78]([-()]*\d){10}$/;
-        this.pattern.email = this.pattern.email ? this.pattern.email : /^\w+@\w+\.\w{2,}$/;
+    setPattern() {
 
-
-        
+        if (!this.pattern.name) {
+            this.pattern.name = /^[а-яА-Я]+$/i;
+        }
+        if (!this.pattern.message) {
+            this.pattern.message = /^[а-яА-Я ]+$/i;
+        }
+        if (!this.pattern.phone) {
+            this.pattern.phone = /^\+?[78]([-()]*\d){10}$/;
+        }
+        if (!this.pattern.email) {
+            this.pattern.email = /^\w+@\w+\.\w{2,}$/;
+        }
     }
 }
